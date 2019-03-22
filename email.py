76c475cb6d0d5b19b84aa343ce485694bb23aa18
yunusemre-uzun <yunus.uzun@metu.edu.tr>
2019-03-22 23:26:03 +0300

    bugs fixed

--- email.py
+++ email.py
@@ -2,12 +2,19 @@ inp = input()
 ret = ""
 temp = ""
 flag = 1
+home_flag = 0
 for i in range(len(inp)):
     if (inp[i]=='['):
+        if(home_flag):
+            ret = temp + ret
+            temp = ""
+            continue
         flag = 0
+        home_flag = 1
         continue
     elif (inp[i]==']'):
         flag = 1
+        home_flag = 0
         ret = temp + ret
         temp = ""
         continue
@@ -16,5 +23,7 @@ for i in range(len(inp)):
             ret = ret + inp[i]
         else:
             temp = temp + inp[i]
+if(len(temp) != 0):
+    ret = temp +ret
 
 print(ret)
