eeb8635e8fc80ab56ffda33bde37c32425252be5
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-03-23 16:40:12 +0300

    Clock bugs fixed + changes due to errors

--- clock.py
+++ clock.py
@@ -13,39 +13,33 @@ Sample Output
 0.7696
 """
 
+# if speeds[i] == 0:
 
 def strangeClock(L, T, speeds):
     if not speeds or L == 0:
-        print(0)
+        print("{0:0.7f}".format(0))
     elif T == 0:
         failProb = ((L-1)/L) ** len(speeds)
-        print(1 - failProb)
+        print("{0:0.7f}".format(1 - failProb))
     else:
         failProb = .0
         flag = False
         lens = len(speeds)
         for i in range(lens):
-            if speeds[i] == 0:
+            x = abs(T * speeds[i])
+            if x < L:
                 if not flag:
                     flag = True
-                    failProb = (L-1)/L
+                    failProb = (L - x) / L
                 else:
-                    failProb *= (L-1)/L
-            else:
-                x = abs(T * speeds[i])
-                if x < L:
-                    if not flag:
-                        flag = True
-                        failProb = (L - x) / L
-                    else:
-                        failProb *= (L - x) / L
-        print(1 - failProb)
+                    failProb *= (L - x) / L
+        print("{0:0.7f}".format(1 - failProb))
 
 
 if __name__ == '__main__':
     speeds = []
     inp = list(map(int, input().split(" ")))
-    for i in range(inp[1]):
+    for _ in range(inp[1]):
         speeds.append(int(input()))
     T = int(input())
     strangeClock(inp[0], T, speeds)
